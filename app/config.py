eef7f252e7170f97f0f7a584bf029ada27965f57
SophiaNM <njerimurage92@outlook.com>
2018-06-14 15:29:03 +0300

    Adding Production and Development Configurations

---
+++ app/config.py
@@ -0,0 +1,27 @@
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SOURCE_API_BASE_URL ='https://newsapi.org/v2/sources?{}&apiKey={}'
+
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
