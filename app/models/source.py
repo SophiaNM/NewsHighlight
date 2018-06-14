d21dc0b2225a132d0a769c2e7d1b2efe9e887625
SophiaNM <njerimurage92@outlook.com>
2018-06-14 16:06:41 +0300

    creating new Source class and declaring objects

---
+++ app/models/source.py
@@ -0,0 +1,11 @@
+class Source:
+    '''
+    Source class to define News Source Objects
+    '''
+    def __init__(self,id,name,description,url,category,country):
+        self.id =id
+        self.name =name
+        self.description =description
+        self.url =url
+        self.category =category
+        self.country =country
