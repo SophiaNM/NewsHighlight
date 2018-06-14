a3419588d90c54e275d7eaa53a7411010c35c4be
SophiaNM <njerimurage92@outlook.com>
2018-06-14 16:07:22 +0300

    Creating second test case to test initializing behaviour

--- app/test_newsource.py
+++ app/test_newsource.py
@@ -1,5 +1,5 @@
 import unittest
-from .models import source
+from models import source
 Source = source.Source
 
 class SourceTest(unittest.TestCase):
@@ -11,11 +11,24 @@ class SourceTest(unittest.TestCase):
         '''
         Set up method that will run before every Test
         '''
-        self.new_source = Source()
+        self.new_source = Source('abc-news','ABC News','Your trusted source for breaking news.','http://abcnews.go.com/','general','us')
 
     def test_instance(self):
+        '''
+        test case to test if object instance is created
+        '''
         self.assertTrue(isinstance(self.new_source,Source))
 
+    def test_init(self):
+        '''
+        Test case to test if object is initialized properly
+        '''
+        self.assertEqual(self.new_source.id,'abc-news')
+        self.assertEqual(self.new_source.name,'ABC News')
+        self.assertEqual(self.new_source.description,'Your trusted source for breaking news.')
+        self.assertEqual(self.new_source.url,'http://abcnews.go.com/')
+        self.assertEqual(self.new_source.category,'general')
+        self.assertEqual(self.new_source.country,'us')
 
 if __name__ == '__main__':
     unittest.main()
