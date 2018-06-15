88e533f748e5bbc14912eee9711360b6010dff03
SophiaNM <njerimurage92@outlook.com>
2018-06-15 23:16:10 +0300

    Added function to process results for news source

--- app/request.py
+++ app/request.py
@@ -1,5 +1,54 @@
 from app import app
+import urllib.request,json
+from .models import source
+
+Source = source.Source
 
 # Getting api key
-
 api_key = app.config['NEWS_API_KEY']
+
+#Getting the movie base url
+base_url = app.config["SOURCE_API_BASE_URL"]
+
+def get_newsource(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_newsource_url = base_url.format(category,api_key)
+    print(get_newsource_url)
+
+    with urllib.request.urlopen(get_newsource_url) as url:
+        get_newsource_data = url.read()
+        get_newsource_response = json.loads(get_newsource_data)
+
+        newsource_results = None
+
+        if get_newsource_response['sources']:
+            newsource_results_list = get_newsource_response['sources']
+            newsource_results = process_results(newsource_results_list)
+
+    return newsource_results
+
+def process_results(newsource_list):
+    '''
+    Function  that processes the new source result and transform them to a list of Objects
+
+    Args:
+        newsource_list: A list of dictionaries that contain movie details
+
+    Returns :
+        newsource_results: A list of movie objects
+    '''
+    newsource_results = []
+    for news_item in newsource_list:
+        id = news_item.get('id')
+        name = news_item.get('name')
+        description = news_item.get('description')
+        url = news_item.get('url')
+        category = news_item.get('category')
+        country = news_item.get('country')
+
+        newsource_object = Source(id,name,description,url,category,country)
+        newsource_results.append(newsource_object)
+
+    return newsource_results
