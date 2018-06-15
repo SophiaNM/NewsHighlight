008452b9c62bef8346e946c810d264b078f19b2b
SophiaNM <njerimurage92@outlook.com>
2018-06-15 23:18:14 +0300

    added other categories for getting newsources

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_newsource
 
 # Views
 @app.route('/')
@@ -8,5 +9,13 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
+    #Getting news sources
+    general_newsource = get_newsource('general')
+    technology_newsource = get_newsource('technology')
+    entertainment_newsource = get_newsource('entertainment')
+    sports_newsource = get_newsource('sports')
+    business_newsource = get_newsource('business')
+    science_newsource = get_newsource('science')
+
     title = 'Home | New Highlights'
-    return render_template('index.html', title=title)
+    return render_template('index.html', title=title, general = general_newsource, technology = technology_newsource, entertainment = entertainment_newsource, sports = sports_newsource, business= business_newsource, science = science_newsource)
