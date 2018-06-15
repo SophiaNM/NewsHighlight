68732c5c8a9757930d7c075f7a0c88bfd4d9a508
SophiaNM <njerimurage92@outlook.com>
2018-06-15 23:16:56 +0300

    added source API url

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ class Config:
     '''
     General configuration parent class
     '''
-    SOURCE_API_BASE_URL ='https://newsapi.org/v2/sources?{}&apiKey={}'
+    SOURCE_API_BASE_URL ='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
 
 
 
