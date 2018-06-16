352a3e23bf09063eb20afda321f5d47b42d2c17e
SophiaNM <njerimurage92@outlook.com>
2018-06-16 14:53:05 +0300

    Adding the articles base url

--- app/config.py
+++ app/config.py
@@ -3,6 +3,9 @@ class Config:
     General configuration parent class
     '''
     SOURCE_API_BASE_URL ='https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+    EVERYTHING_SOURCE_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&pageSize={}&apiKey={}'
+    TOP_HEADLINES_BASE_URL = 'https://newsapi.org/v2/top-headlines?language=en&pageSize=40&apiKey={}'
+    EVERYTHING_BASE_URL = 'https://newsapi.org/v2/everything?sources=bbc-news,al-jazeera-english,cnn,independent,google-news,the-telegraph,mashable,the-lad-bible,buzzfeed,bloomberg,engadget,espn,fortune&sortBy=publishedAt&pageSize=40&apiKey={}'
 
 
 
