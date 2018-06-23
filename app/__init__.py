923ce7ee5cae4ce08b8a7ac86097672494743ae2
SophiaNM <njerimurage92@outlook.com>
2018-06-24 01:52:09 +0300

    Initializing bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,44 @@
+# from flask import Flask
+# from .config import DevConfig
+# from flask_bootstrap import Bootstrap
+#
+# # Initializing application
+# app = Flask(__name__,instance_relative_config = True)
+#
+# # Setting up configuration
+# app.config.from_object(DevConfig)
+# app.config.from_pyfile('config.py')
+#
+# # Initializing Flask Extensions
+# bootstrap = Bootstrap(app)
+#
+# from app import views
+
 from flask import Flask
-from .config import DevConfig
 from flask_bootstrap import Bootstrap
+from config import config_options
 
-# Initializing application
-app = Flask(__name__,instance_relative_config = True)
+bootstrap = Bootstrap()
 
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+def create_app(config_name):
 
-# Initializing Flask Extensions
-bootstrap = Bootstrap(app)
+    app = Flask(__name__)
 
-from app import views
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    # setting config
+    from .request import configure_request
+    configure_request(app)
+
+    # Will add the views and forms
+
+
+    return app
